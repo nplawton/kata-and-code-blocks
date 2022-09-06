529b04c862ac3ed3c9ce5e00db606508b3f3f30c
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-09-06 10:12:41 -0500

    Added reverse string code block

---
+++ reverse-string.js
@@ -0,0 +1,15 @@
+// create a function that will take in a string as an argument and output a reversed string
+
+function reverseStr (str) {
+    // create variable to store reversed string
+    var revStr = '';
+    // loop through the string and go through each character in the string backwards, starting with the last character
+    for (var i = str.length - 1; i >= 0; i--) {
+        // within the loop, concatenate each letter in string to variable declared earlier
+        revStr += str[i];
+    }
+    // log reversed string
+    console.log(revStr);
+}
+
+reverseStr("I want to be reversed");
