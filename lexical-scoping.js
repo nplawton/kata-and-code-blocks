5b56a9eea1be5707387c2f8edeb4aaf5d6767067
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-04 09:23:01 -0500

    Updated instructions

--- lexical-scoping.js
+++ lexical-scoping.js
@@ -1,7 +1,7 @@
 // This is a great example of scoping
 // based on this simple code block we can see how scoping works with JavaScript
 // Analyze the code and see if you can guess what this code logs
-// To give it a try, invoke function two() it in your terminal with Node
+// To give it a try, run it in your terminal with Node
 // To do that (with Node installed on your machine) type 'node lexical-scoping.js'
 
 const num = 6;
@@ -17,3 +17,5 @@ function two() {
 };
 
 two()
+
+// Were you right?
