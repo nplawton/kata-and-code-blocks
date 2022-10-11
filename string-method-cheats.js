3644056b566a2bf21665c616405254bba7844d86
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-10-11 15:25:20 -0500

    Added a bunch more string methods, nice cheat sheet

--- string-method-cheats.js
+++ string-method-cheats.js
@@ -5,9 +5,9 @@ console.log(txt.charAt(0));
 
 // concat();
 const str1 = "Hello ";                                          // These are just a collection of string methods I like to keep handy
-const str2 = "Code Buddy";                                      // Try them yourself by running this file in Node
-console.log(str1.concat(str2));
-// prints 'Hello Code Buddy'
+const str2 = "Code Buddy";                                      // I used 'strX' throughout so each code block would run
+console.log(str1.concat(str2));                                 // What's your favorite one?
+// prints 'Hello Code Buddy'                                    // Try them yourself by running this file wwith Node
 
 // indexOf();
 const text = "Let's find where 'pen' occurs!";
@@ -40,3 +40,43 @@ const s = "JavaScript";
 console.log(s.substr(4, 6));
 // prints Script
 
+// toUpperCase()
+const phrase = "CodingHero";
+console.log(phrase.toUpperCase());
+// prints CODINGHERO
+
+// valueOf()
+const str6 = new String('test');
+console.log(typeof str6); // 'object'
+console.log(str6.valueOf()); // 'test'
+console.log(typeof str6.valueOf()); // 'string'
+
+// trim()
+const str7 = "      Trim both sides      ";
+console.log(str7.trim());
+// prints Trim both sides (without the spaces at front and rear)
+
+// toString()
+const myNum = 155;
+console.log(myNum.toString());
+// prints 155 as a string instead of a number
+
+// includes()
+const str8 = "My name is Kev the Dev";
+console.log(str8.includes("name"));
+// prints true
+
+//charCodeAt()
+const str9 = "TEST";
+console.log(str9.charCodeAt(0));
+// prints 84
+
+// match()
+const str10 = "lopersum lopersum lopersum lopersum";
+console.log(str10.match(/sum/g));
+// prints [ 'sum', 'sum', 'sum', 'sum' ] as an array
+
+// split()
+const str11 = "1, 2, 3, 4, 5";
+console.log(str11.split(","));
+// prints [ '1', ' 2', ' 3', ' 4', ' 5' ]
